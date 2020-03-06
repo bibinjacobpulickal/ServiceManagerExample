fe9f7460d07e43091ddc457b01023617d6f96e62
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-03-06 14:22:03 +0530

    Initial Commit

---
+++ ServiceManagerExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ServiceManagerExample
+//
+//  Created by Bibin Jacob Pulickal on 06/03/20.
+//  Copyright © 2020 Bibin Jacob Pulickal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
